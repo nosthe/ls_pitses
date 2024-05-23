57ff0f28c14af10e8be154e8380f5f1d91472444
nosthe <155254803+nosthe@users.noreply.github.com>
2024-05-23 23:25:30 +0300

    to header me tis metavlites
    den einai plhres kai thelei mallon kapoio kolpo me #define

---
+++ pizza.h
@@ -0,0 +1,14 @@
+
+
+int Ntel=2;
+int Ncook=2;
+int Noven=10;
+int Ndeliverer=10;
+int Ncust;
+int seed;
+
+int thlefwnites=Ntel;
+int mageires=Ncook;
+int fournoi=Noven;
+int dianomeis=Ndeliverer;
+
